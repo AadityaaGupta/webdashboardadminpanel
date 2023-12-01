638f654d6fd8a7ff29a09facd7cba63cd6ac0d04
Aaditya Gupta <adeethgupta@gmil.com>
2023-11-30 23:55:06 -0800

    some

---
+++ lib/Login.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+            body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: height,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/img.jpg"),
+                      fit: BoxFit.cover)),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 80.0,
+                  ),
+                  Center(
+                    //Login design starts from here
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                          padding: EdgeInsets.only(top: height * 0.10),
+                          height: height * 0.7,
+                          width: width * 0.5,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage("assets/images/img.jpg"),
+                                  fit: BoxFit.cover,
+                                  opacity: 0.5,
+                                  ),
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.white,
+                                  blurRadius: 6.0,
+                                  spreadRadius: 2.0,
+                                  offset: Offset(0.0, 0.0),
+                                )
+                              ]),
+                          child: Column(children: <Widget>[
+                            Center(
+                              child: Text(
+                                'Login Form',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 40.0,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Center(
+                              child: SizedBox(
+                                width: width * 0.4,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "Enter Username",
+                                      hintStyle: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold)),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Center(
+                              child: SizedBox(
+                                width: width * 0.4,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      hintText: "Enter Pasword",
+                                      hintStyle: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Center(
+                                child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text('Login',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  )),
+                            )),
+                          ])),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  
+  }
+}
