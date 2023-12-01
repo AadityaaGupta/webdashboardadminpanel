9e660a64926fcc78647bd4b33fd2c56f551a78fc
Aaditya Gupta <adeethgupta@gmil.com>
2023-12-01 04:35:16 -0800

    changes

--- lib/dashboard_sendnotification_screen.dart
+++ lib/dashboard_sendnotification_screen.dart
@@ -8,47 +8,85 @@ class SendNotificationScreen extends StatefulWidget {
 }
 
 class _SendNotificationScreenState extends State<SendNotificationScreen> {
+  TextEditingController _field = TextEditingController();
+  TextEditingController _field1 = TextEditingController();
+  TextEditingController editingController = TextEditingController();
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(//Main Container
-        width: width*0.8,
+      child: Container(
+        //Main Container
+        width: width * 0.8,
         height: height,
-      
+
         child: Column(
-      
-            children: [
-                          //Search field Starts from here
-                Row(
-                  children: [
-                    Center(
-                        child: 
-                        Container(
-                          decoration: BoxDecoration(
-                border: Border.all(
-                  width: 2.0,
-                  color: Colors.red,
-                )
-              ),
-                              height: height*0.1,
-                              width: width*0.5,
-                              child: TextField(),
+          children: [
+            //Search field Starts from here
+            Row(
+              children: [
+                Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      width: 1.0,
+                      color: Colors.red,
+                    )),
+                    height: height * 0.08,
+                    width: width * 0.5,
+                    // alignment: Alignment.center,
+                    child: TextField(
+                        
+                      controller: _field,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        suffixIcon: const Icon(
+                        Icons.search,
+                        size: 30.0,
+                      ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
                         ),
-                        
-                        
+                        hintText: 'Selected User',
+                        hintStyle: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ],
+                  ),
                 ),
-      
-            ],
-      
+                SizedBox(
+                  width: width * 0.02,
+                ),
+                Center(
+                  child: Container(
+                    child: ElevatedButton(
+
+                      child: Text('Pick',),
+                      onPressed: () {
+
+
+
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
-      
       ),
     );
-
   }
+} 
+
+class User
+{
+  
 }
